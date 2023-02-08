33d34d07d3458f49915ab72c18cef74f0d9205fd
Ananya Nayak <55504616+Ananya2001-an@users.noreply.github.com>
2023-02-08 11:53:40 +0000

    added hashrouter

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<BrowserRouter>
+	<HashRouter basename='/'>
 		<App />
+	</HashRouter>
 	</BrowserRouter>
 )
