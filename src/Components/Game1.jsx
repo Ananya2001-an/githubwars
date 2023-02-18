4276f9fcfdd4eabe7d615c4d6714a8ee80f0bad0
Jason <jasondsouza212@gmail.com>
2023-02-18 16:08:23 +0530

    user1's Local storage

--- src/Components/Game1.jsx
+++ src/Components/Game1.jsx
@@ -1,4 +1,4 @@
-import {useRef, useState} from 'react';
+import {useRef, useState, useEffect} from 'react';
 import {Button, Form, InputGroup, Alert} from 'react-bootstrap';
 import {useNavigate} from 'react-router-dom';
 import {FaGithub} from 'react-icons/fa';
@@ -9,13 +9,13 @@ export default function Game1() {
 	const [error, setError] = useState('');
 	const [hideInput1, setHideInput1] = useState(false);
 	const [hideInput2, setHideInput2] = useState(true);
+	const [inputValue, setInputValue] = useState('');
 	const username1Ref = useRef();
 	const username2Ref = useRef();
 	const navigate = useNavigate();
 	const headers = {
 		authorization: `token ${process.env.REACT_APP_PAT}`,
 	};
-
 	const fetchUser = (user_no) => {
 		fetch(
 			`https://api.github.com/users/${
@@ -45,6 +45,18 @@ export default function Game1() {
 		navigate('/stats', {state: {user1, user2}});
 	};
 
+	useEffect(() => {
+		const storedValue = localStorage.getItem('myData');
+		if (storedValue) {
+		setInputValue(storedValue);
+		}
+	}, []);
+
+	const handleInputChange = (event) => {
+		const value = event.target.value;
+		setInputValue(value);
+		localStorage.setItem('myData', value);
+	};
 	return (
 		<>
 			<div className='bg'>
@@ -78,6 +90,9 @@ export default function Game1() {
 								ref={username1Ref}
 								placeholder='Enter 1st GitHub Username'
 								style={{background: 'transparent'}}
+								value={inputValue}
+								onChange={handleInputChange}
+								
 							/>
 							<Button className='btn' onClick={() => fetchUser('1')}>
 								Get
